6160898dc3b91a3073e641bbe0280664d6e4eb3a
Yevhenii Kostenko <Atom903@gmail.com>
2022-01-15 13:12:56 +0200

    [+] created project
    [+] add sprites and sounds
    [+] start to create game scene
    [+] add core utilities

---
+++ HopBallProject/Assets/Scripts/Core/Utilities/EventAggregator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Core.Utilities
+
+{
+    public static class EventAggregator
+    {
+        public static void Subscribe<T>(Action<object, T> @event)
+        {
+            EventHolder<T>.Event += @event;
+        }
+
+        public static void Unsubscribe<T>(Action<object, T> @event)
+        {
+            EventHolder<T>.Event -= @event;
+        }
+
+        public static void Post<T>(object sender, T eventData)
+        {
+            EventHolder<T>.Post(sender, eventData);
+        }
+
+        private static class EventHolder<T>
+        {
+            public static event Action<object, T> Event;
+            public static void Post(object sender, T eventData)
+            {
+                Event?.Invoke(sender, eventData);
+            }
+        }
+    }
+}
